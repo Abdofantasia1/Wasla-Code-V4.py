45f06343d642c4c486f274b615025ac9ac5d1d55
Abdofantasia1 <74551472+Abdofantasia1@users.noreply.github.com>
2020-11-16 16:53:37 +0200

    Wasla code V4

---
+++ Wasla V4.py
@@ -0,0 +1,151 @@
+import threading 
+import os,sys,time
+import webbrowser
+def jalan(z):
+ for e in z + '\n':
+  sys.stdout.write(e)
+  sys.stdout.flush()
+  time.sleep(00000.05)
+jalan('\033[1;32m Welcome To in Script Wasla code')
+logo=''' \033[1;33m
+
+███████╗░█████╗░██████╗░░█████╗░███╗░░██╗████████╗
+██╔════╝██╔══██╗██╔══██╗██╔══██╗████╗░██║╚══██╔══╝
+█████╗░░███████║██████╔╝██║░░██║██╔██╗██║░░░██║░░░
+██╔══╝░░██╔══██║██╔══██╗██║░░██║██║╚████║░░░██║░░░
+██║░░░░░██║░░██║██║░░██║╚█████╔╝██║░╚███║░░░██║░░░
+╚═╝░░░░░╚═╝░░╚═╝╚═╝░░╚═╝░╚════╝░╚═╝░░╚══╝░░░╚═╝░░░
+
+
+'''
+jalan(logo)
+log='''
+███╗░░██╗███████╗████████╗
+████╗░██║██╔════╝╚══██╔══╝
+██╔██╗██║█████╗░░░░░██║░░░
+██║╚████║██╔══╝░░░░░██║░░░
+██║░╚███║███████╗░░░██║░░░
+╚═╝░░╚══╝╚══════╝░░░╚═╝░░░
+████████╗███████╗░█████╗░███╗░░░███╗
+╚══██╔══╝██╔════╝██╔══██╗████╗░████║
+░░░██║░░░█████╗░░███████║██╔████╔██║
+░░░██║░░░██╔══╝░░██╔══██║██║╚██╔╝██║
+░░░██║░░░███████╗██║░░██║██║░╚═╝░██║
+░░░╚═╝░░░╚══════╝╚═╝░░╚═╝╚═╝░░░░░╚═╝
+'''
+jalan(log)
+jalan('\033[1;34m')
+jalan('Telegram : FaRoN Team')
+jalan('Telegram : NeT Team')
+jalan('YouTube  : FaRoN')
+jalan('> 17 code < ')
+jalan('\033[1;31m##################### Wasla code #####################')
+jalan('\033[1;35m')
+
+new=input('Enter Pass : ')
+if new=='Abdo' :
+	jalan('\033[1;36mNice..!')
+	jalan('')
+	for i in range(1):
+		print('egAKoJ29')
+		time.sleep(1)
+		
+		
+	for i in range(1):
+		print('egZwxPEn')
+		time.sleep(1)
+		
+	for i in range(1):
+		print('MByKGr')
+		time.sleep(1)
+	
+		
+	for i in range(1):
+		print('egAyWrpk')
+		time.sleep(1)
+		
+
+	for i in range(1):
+		print('egAQ1zqA')
+		time.sleep(1)
+		
+		
+		
+	for i in range(1):
+		print('egZ49wgn')
+		time.sleep(1)
+		
+		
+	for i in range(1):
+		print('egebWPq1')
+		time.sleep(1)
+		
+		
+	for i in range(1):
+		print('egZDKPxy')
+		time.sleep(1)
+		
+		
+	
+
+	for i in range(1):
+		print('DoQPde')
+		time.sleep(1)
+		
+		
+	
+	for i in range(1):
+		print('egAQXGRq')
+		time.sleep(1)
+	
+	
+	
+	
+	for i in range(1):
+		print('egZ1947v')
+		time.sleep(1)
+		
+		
+	for i in range(1):
+		print('ege8J6Qd')
+		time.sleep(1)
+		
+		
+
+	for i in range(1):
+		print('MdREGd')
+		time.sleep(1)
+		
+			
+				
+				
+				
+				
+	for i in range(1):
+		print('egZmP2Eo')
+		time.sleep(1)
+		
+	for i in range(1):
+		print('DNGOON')
+		time.sleep(1)
+		
+		
+	for i in range(1):
+		print('egZ7q6w6')
+		time.sleep(1)
+		
+
+
+
+	for i in range(1):
+		print('DQvgNL')
+		time.sleep(1)
+
+
+	jalan('انتهت الاكواد')
+	
+
+else:
+	jalan('ERROR Pass..!')
+	webbrowser.open('https://t.me/FaRoN_Team')
+	
